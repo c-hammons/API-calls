9045b9aac7b88c4babfa8dd10bc310f7d34d0276
c-hammons <hammonscassandra@gmail.com>
2022-04-04 21:01:58 -0400

    creating data, accessing api

--- trendsAPI.py
+++ trendsAPI.py
@@ -3,13 +3,20 @@ import pandas as pd
 from pytrends.request import TrendReq
 
 
-pytrend = TrendReq(hl='en-US')
-drugs = ['Avalide', 'Avapro', 'Coumadin', 'Eliquis', 'Plavix', 'Pravachol', 'Bydureon', 'Byetta', 'Farxiga/Forxiga', 'Glucophage',
+
+drugs = ['Avalide', 'Avapro', 'Coumadin', 'Eliquis', 'Plavix', 'Pravachol', 'Bydureon', 'Byetta', 'Farxiga', 'Glucophage',
           'Glucophage XR', 'Glucovance', 'Kombiglyze', 'Onglyza', 'Atripla', 'Azactam', 'Baraclude', 'Daklinza', 'Evotaz', 'Megace',
-          'Reyataz', 'Sustiva', 'Videx', 'Videx EC', 'Zeri', 'Kenalog-10', 'Kenalog-40', 'BiCNU', 'CeeNU', 'Droxia/Hydrea', 'Empliciti',
+          'Reyataz', 'Sustiva', 'Videx', 'Videx EC', 'Zeri', 'Kenalog-10', 'Kenalog-40', 'BiCNU', 'CeeNU', 'Droxia', 'Empliciti',
           'Erbitux', 'Etopophos', 'Ixempra', 'Lysodren', 'Megace', 'Opdivo', 'Sprycel',  'Taxol', 'Vumon',
-          'Yervoy', 'Abilify', 'Orencia', 'Nulojix', 'Bristol Meyers Squibb']
-kw = drugs[0]
-df = pd.DataFrame()
-pytrend.build_payload(kw_list=['google'], cat=0, timeframe='today 5-y', geo='US')
-df = pytrends.interest_over_time(kw_list=['Avalide'])
+          'Yervoy', 'Abilify', 'Orencia', 'Nulojix']
+
+for drug in drugs:
+    pytrends = TrendReq(hl='en-US')
+    keyword = []
+    keyword.append(drug)
+    pytrends.build_payload(kw_list=keyword, cat=0, timeframe='today 5-y', geo='US')
+    df = pytrends.interest_over_time()
+    drug_info = []
+    drug_info.append(df)
+
+
